3048159cbfa458df5e671c29d2f98ecdf66796e3
Gurleen Sethi <sarusethi@rocketmail.com>
2020-03-09 22:17:06 -0400

    feat: add movie search store.

---
+++ src/store/MovieSearchStore.tsx
@@ -0,0 +1,8 @@
+import { observable } from "mobx";
+
+export class MovieSearchStore {
+  @observable term: string = "";
+  @observable status: string = "";
+  @observable.shallow results: any[] = [];
+  @observable totalCount: number = 0;
+}
